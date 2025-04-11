a0bc6d30d1cf78617ec767355d0d54407b76a207
白神遥真可爱 <292200693@qq.com>
2025-04-11 20:34:29 +0800

    整理代码

--- src/main/java/cn/chengzhiya/mhdfoptimize/hook/hook/AbstractHook.java
+++
@@ -1,9 +0,0 @@
-package cn.chengzhiya.mhdfoptimize.hook.hook;
-
-import cn.chengzhiya.mhdfoptimize.interfaces.Hook;
-import lombok.Getter;
-
-@Getter
-public abstract class AbstractHook implements Hook {
-    public boolean enable = false;
-}
